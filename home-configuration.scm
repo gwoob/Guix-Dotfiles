3699b36d11065f6068b0167e28343df44c1263a5
b c <47470625+gwoob@users.noreply.github.com>
2024-06-21 06:20:54 +0000

    Create home-configuration.scm

---
+++ home-configuration.scm
@@ -0,0 +1,44 @@
+;; This "home-environment" file can be passed to 'guix home reconfigure'
+;; to reproduce the content of your profile.  This is "symbolic": it only
+;; specifies package names.  To reproduce the exact same profile, you also
+;; need to capture the channels being used, as returned by "guix describe".
+;; See the "Replicating Guix" section in the manual.
+
+(use-modules (gnu home)
+             (gnu packages)
+             (gnu services)
+             (guix gexp)
+	     (gnu home services desktop)
+             (gnu home services shells)
+	     (gnu home services sound))
+
+(home-environment
+  ;; Below is the list of packages that will show up in your
+  ;; Home profile, under ~/.guix-home/profile.
+ (packages (specifications->packages (list "wl-clipboard"
+					   "river"
+					   "bemenu"
+					   "librewolf"
+					   "fish"
+                                           "foot"
+                                           "emacs-pgtk"
+					   "xdg-user-dirs"
+					   "pavucontrol")))
+
+  ;; Below is the list of Home services.  To search for available
+  ;; services, run 'guix home search KEYWORD' in a terminal.
+  (services
+   (list
+    (service home-dbus-service-type)
+    (service home-pipewire-service-type))))
+;;    (service home-bash-service-type
+;;                  (home-bash-configuration
+;;                   (aliases '(("grep" . "grep --color=auto")
+;;                              ("ip" . "ip -color=auto")
+;;                              ("ll" . "ls -l")
+;;                              ("ls" . "ls -p --color=auto")))
+;;                   (bashrc (list (local-file
+;;                                  "/home/ben/src/guix-config/.bashrc" "bashrc")))
+;;                   (bash-profile (list (local-file
+;;                                        "/home/ben/src/guix-config/.bash_profile"
+;;                                        "bash_profile"))))))))
